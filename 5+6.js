9eac1daaf9e9a934c8ae696ee993169c1f21afb5
Ruslan <rusishss@mail.ru>
2022-11-09 19:13:57 +0600

    Еще один пробник

--- 5+6.js
+++ 5+6.js
@@ -1,2 +1,11 @@
 //Manipulate Arrays With pop() Управление массивами с помощью pop()
 const myArray = [["John", 23], ["cat", 2]];
+/*Используйте .pop()функцию, чтобы удалить последний элемент myArray
+и присвоить выскочившее значение новой переменной, removedFromMyArray.*/
+const removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray)
+
+const myArray2 = [["John", 23], ["dog", 3]];
+/*Используйте .shift()функцию, чтобы удалить первый элемент из myArray
+и присвоить «смещенное значение» новой переменной, removedFromMyArray.*/
+const removedFromMyArray2 = myArray2.shift();
